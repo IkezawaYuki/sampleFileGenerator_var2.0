2bd50c9fbd93d0fc695aff5fef31130714a5aaba
ikezawa <ikezawa_y@worksap.co.jp>
2019-01-29 20:02:11 +0900

    アークより作業 変換詳細情報のチェックにおいて、無限ループ検出の機能を実装。

--- model/checking.py
+++ model/checking.py
@@ -2,6 +2,7 @@ import tkinter.messagebox
 import math
 import sys
 import pprint
+import json
 
 import generator_config as l
 
@@ -55,6 +56,9 @@ class ConversionDetailedGroup:
     def __bool__(self):
         return self.statement
 
+    def __str__(self):
+        return self.name
+
 
 class Inspect:
     def __init__(self):
@@ -62,28 +66,32 @@ class Inspect:
         self.jump_stack = []
         self.rooting = []
 
-    def cell_scan(self, cell):
+    def cell_scan(self, cell, c_group):
         if ">>" in cell:
-            jump = adjust_togo(cell)
+            jump = self.adjust_togo(cell)
             self.jump_stack.append(jump)
         elif ">" in cell:
-            root = adjust_togo(cell)
-            if root not in self.rooting:
-                return False
-            #todo 無限ループの可能性を検出するロジックの追加。
-
-        return True
+            key = self.adjust_togo(cell)
+            if key not in self.rooting:
+                return 101
+            elif self.reference_row(key, c_group) is False:
+                return 102
+        return 0
 
     def row_scan(self, c_group, c_row, key):
         for num, cell in enumerate(c_row):
             if num == 10:
                 c_row[10] = True
                 continue
-            result = self.cell_scan(cell)
-            if result is False:
+            result = self.cell_scan(cell, c_group)
+            if result == 101:
                 c_group.statement = False
                 c_group.error_msg.append("NullPointerExceptionが発生しています。\n"
-                                         "変換名称 : {}\nレーン {} 順序 {}".format(c_group.name, str(key)[0], str(key)[2]))
+                                         "変換名称 : {}\nレーン {} 順序 {}".format(c_group, str(key)[0], str(key)[2]))
+            elif result == 102:
+                c_group.statement = False
+                c_group.error_msg.append("無限ループが発生する可能性があります。\n"
+                                         "変換名称 : {}\nレーン {} 順序 {}".format(c_group, str(key)[0], str(key)[2]))
         self.rooting.append(key)
 
     def lane_scan(self, c_group, lane):
@@ -91,28 +99,26 @@ class Inspect:
         for key in lane:
             if str(i) != str(key)[-1]:
                 c_group.error_msg.append("処理を飛ばす先が存在しません。\n"
-                                         "変換名称 : {}\nレーン {} 順序 {}".format(c_group.name, str(key)[0], str(key)[2]))
+                                         "変換名称 : {}\nレーン {} 順序 {}".format(c_group, str(key)[0], str(key)[2]))
             target_rows = c_group.conversion_rows[key]
             self.row_scan(c_group, target_rows, key)
             i += 1
 
     def inspect_main(self, group):
-        temp = group.lane.pop(0)
+        temp = group.lane[0]
         self.lane_scan(group, temp)
         while len(self.jump_stack) > 0:
             jump_key = self.jump_stack.pop(0)
-            i = 0
             for lists in group.lane:
                 if jump_key in lists:
                     self.lane_scan(group, lists)
-                    group.lane.pop(i)
                     break
-                i += 1
             else:
                 group.error_msg.append("処理を飛ばす先が存在しません。\n"
-                                       "変換名称 : {}\n飛ばす先のレーン : {}".format(group.name, str(jump_key)[0]))
+                                       "変換名称 : {}\n飛ばす先のレーン : {}".format(group, str(jump_key)[0]))
 
-    def adjust_togo(cell):
+    def adjust_togo(self, cell):
+        print(cell)
         if ">>" in cell:
             n = cell.rfind(">")
             togo = cell[n + 1:]
@@ -124,10 +130,14 @@ class Inspect:
             togo = int(togo.replace("-", "0"))
             return togo
 
-    def __str__(self):
-        return str(self.group_name)
-
-
+    def reference_row(self, key, c_group):
+        temp = c_group.conversion_rows[key]
+        for cell in temp:
+            if isinstance(cell, bool):
+                continue
+            if ">>" in cell:
+                return False
+        return True
 
 
 def create_key(lane, order):
@@ -211,42 +221,41 @@ def read_convert_info(sheet):
     return converte_rows, group_name
 
 
-def reference_row(key, group, g_name):
-    """
-    このメソッドで、参照先の行に問題がないかどうかをチェックする。
-    変換定義の仕様上、ある行が>2-3としていて、そのレーン2、順序3の行に>>4、などとある場合、正しく動作しない。
-    >?-? の先に　>>?　が存在しないかどうかをチェックする必要がある。
-    """
-    temp = group[key]
-    for num, cell in enumerate(temp):
-        if num == 10:
-            return False
-        if ">>" in cell:
-            l.logger.info(g_name + " " + str(key) + " is dangerous logic.")
-            yes = tkinter.messagebox.askyesno('inspect -sample file generator ver3.0-',
-                                         "以下の行を参照しようとする際、問題が起きる可能性があります。\n処理を中断しますか？\n\n変換名称：" + g_name + "\n\n" + str(temp))
-            if yes:
-                sys.exit(0)
-
-
-
-def adjust_togo(cell):
-    """
-    >1-1 や >>2などの値をkeyの形（101などの３桁）に修正するメソッド
-    注意が必要なのは109の次は110ではなく1010になる点
-    :param cell: 「>」「>>」が存在するセル　例）>3-1
-    :return: ３桁の数、key
-    """
-    if ">>" in cell:
-        n = cell.rfind(">")
-        togo = cell[n + 1:]
-        togo = int(togo + "01")
-        return togo
-    elif ">" in cell:
-        n = cell.rfind(">")
-        togo = cell[n + 1:]
-        togo = int(togo.replace("-", "0"))
-        return togo
+# def reference_row(key, group, g_name):
+#     """
+#     このメソッドで、参照先の行に問題がないかどうかをチェックする。
+#     変換定義の仕様上、ある行が>2-3としていて、そのレーン2、順序3の行に>>4、などとある場合、正しく動作しない。
+#     >?-? の先に　>>?　が存在しないかどうかをチェックする必要がある。
+#     """
+#     temp = group[key]
+#     for num, cell in enumerate(temp):
+#         if num == 10:
+#             return False
+#         if ">>" in cell:
+#             l.logger.info(g_name + " " + str(key) + " is dangerous logic.")
+#             yes = tkinter.messagebox.askyesno('inspect -sample file generator ver3.0-',
+#                                          "以下の行を参照しようとする際、問題が起きる可能性があります。\n処理を中断しますか？\n\n変換名称：" + group + "\n\n" + str(temp))
+#             if yes:
+#                 sys.exit(0)
+#
+# #
+# def adjust_togo(cell):
+#     """
+#     >1-1 や >>2などの値をkeyの形（101などの３桁）に修正するメソッド
+#     注意が必要なのは109の次は110ではなく1010になる点
+#     :param cell: 「>」「>>」が存在するセル　例）>3-1
+#     :return: ３桁の数、key
+#     """
+#     if ">>" in cell:
+#         n = cell.rfind(">")
+#         togo = cell[n + 1:]
+#         togo = int(togo + "01")
+#         return togo
+#     elif ">" in cell:
+#         n = cell.rfind(">")
+#         togo = cell[n + 1:]
+#         togo = int(togo.replace("-", "0"))
+#         return togo
 
 
 def execute_coverage_test(sheet):
